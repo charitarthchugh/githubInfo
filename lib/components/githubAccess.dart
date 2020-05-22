347880dbc487a3b4e873dd26c28f5a0dabd9cba2
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2020-05-21 20:39:39 -0400

    Remove unnessecary line

--- lib/components/githubAccess.dart
+++ lib/components/githubAccess.dart
@@ -1,10 +1,9 @@
-import 'package:flutter/material.dart';
+
 import 'package:github/github.dart';
 
 Future<void> main() async {
   /* Create a GitHub Client, with anonymous authentication by default */
-  var github = GitHub();
-  User cc =await github.users.getUser("charitarthchugh");
-  Repository repo = await github.repositories.getRepository(RepositorySlug('user_or_org', 'repo_name'));
+  final github = GitHub();
+  final User cc =await github.users.getUser("charitarthchugh");
   /* Do Something with repo */
 }
