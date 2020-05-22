2a91d14e457af5d6deb053f573cb9576ebc419d3
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2020-05-22 13:02:04 -0400

    Update Repository model, add example file(repos.)

--- lib/components/RepositoryModel.dart
+++ lib/components/RepositoryModel.dart
@@ -1,17 +1,21 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
+import '';
 
 class Repository {
   final String name;
-  final String info;
-  final int stars;
-  final int commits;
-  final int releases;
+  final String description;
+  final String homepage;
+  final String language;
+  final String githubUrl;
 
-  Repository({
-      @required this.name,
-      @required this.info,
-      @required this.stars,
-      @required this.commits,
-      @required this.releases});
-  
+  Repository({@required this.name, @required this.description, @required this.homepage,this.language,this.githubUrl});
+
+  factory Repository.fromJson(Map<String, dynamic> json) {
+    return Repository(
+        name: json['name'],
+        homepage: json['url'],
+        description: json['description'],
+        language: json);
+  }
 }
